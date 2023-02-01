c9d1ae2b7d675bac6a7008c0636ae9e127ac4226
Stanley <78428936+chepkok3@users.noreply.github.com>
2023-02-01 07:06:11 +0300

    Add modified files

--- modules/calculator-code-test.js
+++ modules/calculator-code-test.js
@@ -1,65 +1,65 @@
 const calculator = require('./calculator');
 
 describe('adding', () => {
-  test('2 and 5 are 7', () => {
-    expect(calculator.add(2, 5)).toBe(7);
+  test('4 plus 2 is 6', () => {
+    expect(calculator.add(4, 2)).toBe(6);
   });
 
   test('0 and 0 not correct', () => {
     expect(calculator.add(0, 0)).toBe('The x & y values are 0');
   });
 
-  test('10 and 0 equals 10', () => {
-    expect(calculator.add(10, 0)).toBe(10);
+  test('8 plus 0 equals 8', () => {
+    expect(calculator.add(8, 0)).toBe(8);
   });
 });
 
 describe('substracting', () => {
-  test('5 and 2 are 3', () => {
-    expect(calculator.substract(5, 2)).toBe(3);
+  test('7 subract 4 is 3', () => {
+    expect(calculator.substract(7, 4)).toBe(3);
   });
 
-  test('5 and 0 not correct', () => {
-    expect(calculator.substract(5, 0)).toBe('Result remains ${x}');
+  test('6 minus 0 not correct', () => {
+    expect(calculator.substract(6, 0)).toBe('Result remains ${x}');
   });
 
-  test('5 and 10 equals -5', () => {
-    expect(calculator.substract(5, 10)).toBe(-5);
+  test('3 minus 5 equals -2', () => {
+    expect(calculator.substract(3, 5)).toBe(-2);
   });
 });
 
 describe('dividing', () => {
-  test('6 and 2 is 3', () => {
-    expect(calculator.devide(6, 2)).toBe(3);
+  test('8 divided by 4 is 2', () => {
+    expect(calculator.devide(8, 4)).toBe(2);
   });
 
-  test('5 and 0 is 0', () => {
-    expect(calculator.devide(5, 0)).toBe('y value is 0, the result remains 0');
+  test('7 divided by 0 is 0', () => {
+    expect(calculator.devide(7, 0)).toBe('y value is 0, the result remains 0');
   });
 
-  test('10 and 3 equals 3.3333333333333335', () => {
-    expect(calculator.devide(10, 3)).toBe(3.3333333333333335);
+  test('15 divided by 8 equals 1.875 which is a float', () => {
+    expect(calculator.devide(15, 8)).toBe(1.875);
   });
 
-  test('-10 and 3 equals -3.3333333333333335', () => {
-    expect(calculator.devide(-10, 3)).toBe(-3.3333333333333335);
+  test('15 divided by -8 equals -1.875, negative float', () => {
+    expect(calculator.devide(-15, 8)).toBe(-1.875);
   });
 });
 
 describe('multiplying', () => {
-  test('6 and 2 is 12', () => {
-    expect(calculator.multiply(6, 2)).toBe(12);
+  test('4 multiplied by 4 is 16', () => {
+    expect(calculator.multiply(4, 4)).toBe(16);
   });
 
-  test('5 and 0 is 0', () => {
-    expect(calculator.multiply(5, 0)).toBe('result remains ${y}');
+  test('7 multiplied by 0 is 0', () => {
+    expect(calculator.multiply(7, 0)).toBe('result remains ${y}');
   });
 
-  test('10 and 10 equals 100', () => {
-    expect(calculator.multiply(10, 10)).toBe(100);
+  test('8 multiplied by 8 equals 64', () => {
+    expect(calculator.multiply(8, 8)).toBe(64);
   });
 
-  test('1.55 and 3 equals 4.65', () => {
-    expect(calculator.multiply(1.55, 3)).toBe(4.65);
+  test('0.52 multiplied by 6 equals 3.12', () => {
+    expect(calculator.multiply(0.52, 6)).toBe(3.12);
   });
 });
